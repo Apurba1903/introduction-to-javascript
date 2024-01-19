e4c0ccbee8a4e30301c0c86cb7ef0b4e8431bbb3
Apurba1903 <apurba1903@gmail.com>
2024-01-20 01:40:39 +0600

    fixing

--- variable.js
+++ variable.js
@@ -6,7 +6,8 @@ var temperature = 20;
 // String Type
 var person = 'Apurba Halder';
 var location = 'Dhaka';
-    
+var name = 'Arnob Halder';
+
 // Boolean Type
 var serious = true;
 var isFullMarks = true;
